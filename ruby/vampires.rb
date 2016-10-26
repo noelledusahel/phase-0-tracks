256e0c15522d42b0194532e59971f735096e3262
noelledusahel <barberossa@gmail.com>
2016-10-26 10:25:55 -0400

    add question about health insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,6 +3,7 @@ puts "How Old Are You?"
 
 puts "you're #{age} years old?? Oh yeah ... What's your name?"
 	name = gets.chomp
+
 puts "Hey #{name}! Great to meet you. Oh Yea what year were you born?"
 	year = gets.chomp.to_i
 	
@@ -14,7 +15,7 @@ puts "Hey #{name}! Great to meet you. Oh Yea what year were you born?"
 		puts "Why lie about your age?"
 	end
 
-puts "Hey let me treat you to lunch! Its Italy day at the cafeteria would you like some garlic bread? Yes or No?"
+puts "Hey let me treat you to lunch! Its Italy day at the cafeteria would you like some garlic bread? yes or no?"
 	garlic = gets.chomp
 
 	if garlic == "yes"
@@ -23,5 +24,17 @@ puts "Hey let me treat you to lunch! Its Italy day at the cafeteria would you li
 	elsif garlic == "no"
 		puts "probably a vampire"
 	else 
-		puts "wait what did you say?"
+		puts "wait what did you say? yes or no?"
+	end
+
+puts "Would you like to enroll in our company's health insurance?"
+
+	insurance = gets.chomp
+
+	if insurance == "yes"
+		puts "OK! Go see HR"
+	elsif insurance == "no"
+		puts "probably a vampire"
+	else 
+		puts "you need to make a decision, yes or no?"
 	end
