ecc1d9dfab835e1c393f182cef9da755d1e9383c
noelledusahel <barberossa@gmail.com>
2016-10-26 10:22:27 -0400

    Begin questionaire about vampire identity, ask about age and garlic allergies

---
+++ ruby/vampires.rb
@@ -0,0 +1,27 @@
+puts "How Old Are You?"
+	age = gets.chomp.to_i
+
+puts "you're #{age} years old?? Oh yeah ... What's your name?"
+	name = gets.chomp
+puts "Hey #{name}! Great to meet you. Oh Yea what year were you born?"
+	year = gets.chomp.to_i
+	
+	if age == 2016 - year
+		puts "cool!"
+	elsif age < 2016 - year
+		puts "probably a vampire"
+	else 
+		puts "Why lie about your age?"
+	end
+
+puts "Hey let me treat you to lunch! Its Italy day at the cafeteria would you like some garlic bread? Yes or No?"
+	garlic = gets.chomp
+
+	if garlic == "yes"
+		puts "right on, my treat!"
+
+	elsif garlic == "no"
+		puts "probably a vampire"
+	else 
+		puts "wait what did you say?"
+	end
