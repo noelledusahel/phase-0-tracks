c530103e719e7c263a2ec12580fa91b3014c9126
noelledusahel <barberossa@gmail.com>
2016-10-28 14:58:56 -0400

    create survey with a nil statement

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+puts "What is your hamster's name?"
+	hamster_name = gets.chomp
+
+puts "On a scale of 1 to 10 how loud is your hamster?"
+	noise_level = gets.chomp
+
+puts "What color fur does hamster have?"
+	hamster_color = gets.chomp
+
+puts "Is this an adoptable hamster, yes or no?"
+	adoptable = gets.chomp
+
+puts "How old is your hamster?"
+	hamster_age = gets.chomp
+if hamster_age == ""
+	hamster_age = nil
+end 
