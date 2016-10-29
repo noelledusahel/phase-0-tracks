f0a656d00527d99c9af9f83a79db8125a80ea6cc
noelledusahel <barberossa@gmail.com>
2016-10-29 19:47:22 -0400

    start adding conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,30 +1,33 @@
 puts "How Old Are You?"
 	age = gets.chomp.to_i
 
-puts "you're #{age} years old?? Oh yeah ... What's your name?"
+ puts "you're #{age} years old?? Oh yeah ... What's your name?"
 	name = gets.chomp
 
 puts "Hey #{name}! Great to meet you. Oh Yea what year were you born?"
 	year = gets.chomp.to_i
 	
 	if age == 2016 - year
-		puts "cool!"
+		age = true
+		#puts "cool!"
 	elsif age < 2016 - year
-		puts "probably a vampire"
+		age = false
+		#puts "probably a vampire"
 	else 
-		puts "Why lie about your age?"
+		age = false
+		#puts "Why lie about your age?"
 	end
 
 puts "Hey let me treat you to lunch! Its Italy day at the cafeteria would you like some garlic bread? yes or no?"
 	garlic = gets.chomp
 
 	if garlic == "yes"
-		puts "right on, my treat!"
+		garlic = true
+		#puts "right on, my treat!"
 
 	elsif garlic == "no"
-		puts "probably a vampire"
-	else 
-		puts "wait what did you say? yes or no?"
+		garlic = false
+		#puts "probably a vampire"
 	end
 
 puts "Would you like to enroll in our company's health insurance?"
@@ -32,9 +35,17 @@ puts "Would you like to enroll in our company's health insurance?"
 	insurance = gets.chomp
 
 	if insurance == "yes"
-		puts "OK! Go see HR"
+		insurance = true
+		#puts "OK! Go see HR"
 	elsif insurance == "no"
-		puts "probably a vampire"
+		insurance = false
+		#puts "probably a vampire"
 	else 
-		puts "you need to make a decision, yes or no?"
+		#puts "you need to make a decision, yes or no?"
 	end
+
+if age && garlic && insurance
+	puts "Answer: They are probably not a vampire then."
+elsif !age && !garlic && !insurance
+	puts "Answer: They are most certainly a vampire."
+end
