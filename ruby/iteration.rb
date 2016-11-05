8abed0875a9c253b7dddd48c3466a08c8319f761
noelledusahel <barberossa@gmail.com>
2016-11-04 21:06:41 -0400

    use example calls

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -12,14 +12,14 @@
 
 # puts "Now we learned the capitols of the two most important states of the union! (election years only)"
 
-capitols = ["Sacramento", "Tallahassee", "St. Paul", "Annapolis"]
+# capitols = ["Sacramento", "Tallahassee", "St. Paul", "Annapolis"]
 
-state_pairs = {
-	"Sacramento" => "California",
-	"Tallahassee" => "Florida",
-	"St. Paul" => "Minnesota",
-	"Annapolis" => "Maryland"
-}
+# state_pairs = {
+# 	"Sacramento" => "California",
+# 	"Tallahassee" => "Florida",
+# 	"St. Paul" => "Minnesota",
+# 	"Annapolis" => "Maryland"
+# }
 
 # puts capitols
 
@@ -27,8 +27,55 @@ state_pairs = {
 
 # capitols.map! {|capitol| puts capitol + "!"}
 
-puts state_pairs
+# puts state_pairs
 
-state_pairs.each {|city, state| puts city.reverse.upcase, state.reverse.upcase} 
+# state_pairs.each {|city, state| puts city.reverse.upcase, state.reverse.upcase} 
+
+# arr = ["dogs", "cats", "parrots"]
+
+# hash_new = {
+
+# 	:pet1 => "dogs",
+# 	:pet2 => "cats",
+# 	:pet3 => "parrots"
+# }
+
+# Number 1
+# arr.delete_if {|animal| animal == "dogs" }
+# 	puts arr
+
+# hash_new.delete_if{|type, species| species == "cats"}
+# puts hash_new
+
+# Number 2
+
+# arr.keep_if {|animal| animal =~ /[p]/}
+# puts arr
+
+# hash_new.keep_if {|type, species| species =~ /[g]/}
+# puts hash_new
+
+#Number 3
+
+
+# arr.select! {|animal| animal =~ /[c]/}
+# puts arr
+
+# hash_new.select! {|type, species| species =~ /[o]/}
+# puts hash_new
+
+#number 4
+
+arr1 = [2, 4, 6, 8]
+
+puts arr1.drop_while {|i| i < 6 }
+
+
+
+
+
+
+
+
 
 
