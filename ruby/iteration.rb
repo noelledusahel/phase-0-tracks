f336d14440dc26ef83ef07ba4af7d2f024ec943c
noelledusahel <barberossa@gmail.com>
2016-11-04 20:04:16 -0400

    insert variables as parameters

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,9 @@
 def capitolator
-	puts "Lets learn the capitols!"
+	puts "Lets learn some state capitols!"
+	capitol1 = "Sacramento"
+	capitol2 = "Tallahassee"
+	yield(capitol1, capitol2)
 end 
+
+capitolator { |capitol1, capitol2| puts "The capitol of California is #{capitol1}. 
+The capitol of Florida is #{capitol2}" }
