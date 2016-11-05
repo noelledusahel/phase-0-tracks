25a517de299ac877e201ad3acd33b2ce551d27e3
noelledusahel <barberossa@gmail.com>
2016-11-04 20:06:27 -0400

    add status statements

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+
+puts  "What do you know about state capitols? nothing!"
 def capitolator
 	puts "Lets learn some state capitols!"
 	capitol1 = "Sacramento"
@@ -7,3 +9,5 @@ end
 
 capitolator { |capitol1, capitol2| puts "The capitol of California is #{capitol1}. 
 The capitol of Florida is #{capitol2}" }
+
+puts "Now we learned the capitols of the two most important states of the union! (election years only)"
