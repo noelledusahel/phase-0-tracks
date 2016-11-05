e1ce1ff4c1b98ab1c808ddcc04ed5b36450d3275
noelledusahel <barberossa@gmail.com>
2016-11-04 20:24:00 -0400

    use .map! and .each on an array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,13 +1,34 @@
 
-puts  "What do you know about state capitols? nothing!"
-def capitolator
-	puts "Lets learn some state capitols!"
-	capitol1 = "Sacramento"
-	capitol2 = "Tallahassee"
-	yield(capitol1, capitol2)
-end 
+# puts  "What do you know about state capitols? nothing!"
+# def capitolator
+# 	puts "Lets learn some state capitols!"
+# 	capitol1 = "Sacramento"
+# 	capitol2 = "Tallahassee"
+# 	yield(capitol1, capitol2)
+# end 
 
-capitolator { |capitol1, capitol2| puts "The capitol of California is #{capitol1}. 
-The capitol of Florida is #{capitol2}" }
+# capitolator { |capitol1, capitol2| puts "The capitol of California is #{capitol1}. 
+# The capitol of Florida is #{capitol2}" }
 
-puts "Now we learned the capitols of the two most important states of the union! (election years only)"
+# puts "Now we learned the capitols of the two most important states of the union! (election years only)"
+
+capitols = ["Sacramento", "Tallahassee", "St. Paul", "Annapolis"]
+
+state_pairs = {
+	"Sacramento" => "California",
+	"Tallahassee" => "Florida",
+	"St. Paul" => "Minnesota",
+	"Annapolis" => "Maryland"
+}
+
+# puts capitols
+
+# capitols.each {|capitol| puts capitol.upcase }
+
+# capitols.map! {|capitol| puts capitol + "!"}
+
+puts state_pairs
+
+state_pairs.each {|city, state| puts city.reverse.upcase, state.reverse.upcase} 
+
+
