c0d46a2091f8848cafd724fc9f043fffcb821331
noelledusahel <barberossa@gmail.com>
2016-11-06 20:52:25 -0500

    make changes from string to array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,17 +2,21 @@
 name = "Felicia Torres"
 #create an array
 name = name.split(' ')
+
 #Move Index 0 to index 1 position
 p name.insert(1, name.delete_at(0))
+p full_name = name.join(' ')
 #split characters
-name.map! {|x| x.split('')}
-# 	p x.chars
-# 	vowels = ["a", "e", "i", "o", "u"]
-# 	if x.include?(vowels)
-# 		p x.next
-# end
-p name
+name_arr = full_name.chars
+p name_arr 
+p name_arr.class
 
+name_arr.each do |x|
+	if x.include?("a")
+		x.next
+
+	end
+end
 
 
 
