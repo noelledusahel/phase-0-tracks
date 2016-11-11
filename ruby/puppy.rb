6bf9898f191ac561abc7950435b0ead580def777
noelledusahel <barberossa@gmail.com>
2016-11-11 16:59:54 -0500

    create a array with food

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,7 +1,97 @@
-class Puppy
+#class Puppy
+#
+#  def initialize
+#      puts "Initializing new puppy instance ..."
+#  end
+#
+#  def fetch(toy)
+#    puts "I brought back the #{toy}!"
+#    toy
+#  end
+#
+#  def speak(int)
+#      i = 0
+#      while i < int do
+#          puts "Woof!"
+#      i += 1
+#      end
+#  end
+#
+#  def roll_over
+#    p "roll over"
+#  end
+#
+#  def dog_years(human_years)
+#      $age = human_years * 7
+#      p $age
+#  end
+#
+#  def high_five(num)
+#      i = 0
+#      while i < num do
+#          puts "Good dog!"
+#      i += 1
+#      end
+#  end
+#
+#end
+#
+#puppy = Puppy.new
+#puppy.fetch("ball")
+#puppy.speak(5)
+#puppy.roll_over
+#puppy.dog_years(5)
+#puts "Dogs age is #{$age}"
+#puppy.high_five(3)
+
+class Birds
+
+  def initialize
+     puts "Initializing new bird instance ..."
+  end
+
+  def food(type)
+      puts "You ate a #{type}."
+  end
+
+  def miles_flown(int)
+      puts "You flew #{int} miles!"
+  end
+
 end
 
-Puppy.methods
+bird_count = 0
 
-duchess = Puppy.new
+# fit_bit = {}
 
+ fit_bit = []
+while bird_count < 50
+    birddie = Birds.new
+    #birddie = 50.times{Birds.new}
+    birddie.food("worm")
+    birddie.miles_flown(12)
+    bird_count += 1
+
+   
+    fit_bit << "#{"worm"}"
+   
+end
+ p fit_bit
+# birddie_arr = []
+# num = 0
+    
+#     while num < 50
+#     birddie_arr << Birds.new
+#     num += 1
+#     end
+
+# birddie_arr.each do |bird_count|
+#     bird_count.food("worm")
+#     bird_count.miles_flown("100")
+# end
+
+# p birddie_arr
+
+# fit_bit <<  birddie.food("worm")
+# p fit_bit
+#fit_bit << birddie.miles_flown
