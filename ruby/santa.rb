0157e07b56933cd5baa2c40006d0b33dea722305
noelledusahel <barberossa@gmail.com>
2016-11-13 15:38:40 -0500

    create method that pulls reindeer to back of line

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,20 +16,59 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "What a good #{cookie_type} cookie!"
 	end
+
+	# def celebrate_birthday
+	# 	new_age = @age + 1
+	# 	puts "Santa is #{new_age} years old" 
+	# end
+
+	def get_mad_at(reindeer_name)
+		puts "Bad #{reindeer_name}!!"
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+		p @reindeer_ranking
+		
+	end
+	#First the getter code
+	def gender
+		@gender
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+	# then the setter code!
+	def gender= (new_gender)
+		@gender = new_gender
+	end 
+
+	def ethnicity= (new_ethnicity)
+		@ethnicity = new_ethnicity
+	end	
 end
+
+santa = Santa.new("agender","Ethiopian")
+
+puts "Santa is a #{santa.ethnicity} #{santa.gender} person"
+puts "Santa is a #{santa.ethnicity} #{santa.gender} person"
+
+santa.gender = "lesbian"
+santa.ethnicity = "white"
+santa.get_mad_at("Dancer")
+
 
 #Just created a list of names to assign gender and ethnicity attributes
-names = ["Sammie Claus", "Tomas Claus", "Seva Claus", "Jamal Claus"]
-genders = ["agender", "female", "bigender", "male"]
-ethnicities = ["black", "Latino", "Indian", "Japanese-African"]
-#This is a list that will store the names and attributes of applicants to Regency Mall Santa Land 
-diversity_file = []
+# names = ["Sammie Claus", "Tomas Claus", "Seva Claus", "Jamal Claus"]
+# genders = ["agender", "female", "bigender", "male"]
+# ethnicities = ["black", "Latino", "Indian", "Japanese-African"]
+# #This is a list that will store the names and attributes of applicants to Regency Mall Santa Land 
+# diversity_file = []
 
-names.length.times do |i|
-	diversity_file << Santa.new(genders[i], ethnicities[i])
-	puts "Applicant No. #{diversity_file.length}, #{names[i]} is #{genders[i]}, and #{ethnicities[i]}"
-	puts "-------"
-end
+# names.length.times do |i|
+# 	diversity_file << Santa.new(genders[i], ethnicities[i])
+# 	puts "Applicant No. #{diversity_file.length}, #{names[i]} is #{genders[i]}, and #{ethnicities[i]}"
+# 	puts "-------"
+# end
 
 
 
