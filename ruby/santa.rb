f493cc6a0dce142eea7dde92d6325389d49d1f0d
noelledusahel <barberossa@gmail.com>
2016-11-13 11:08:15 -0500

    print applicant names, attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,10 @@
 class Santa
-	def initialize
-		puts "Initializing Santa instance..."
+#Here the Santa Class has been initialized, and the variables gender and ethnicity are passed in...
+	def initialize(gender, ethnicity)
+	# 	puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
+		#these could be used, but i chose not to use these santa defaults
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0 
 	end
@@ -14,9 +16,22 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "What a good #{cookie_type} cookie!"
 	end
-
-
 end
 
-santa = Santa.new
-santa.eat_milk_and_cookies("sugar")
+#Just created a list of names to assign gender and ethnicity attributes
+names = ["Sammie Claus", "Tomas Claus", "Seva Claus", "Jamal Claus"]
+genders = ["agender", "female", "bigender", "male"]
+ethnicities = ["black", "Latino", "Indian", "Japanese-African"]
+#This is a list that will store the names and attributes of applicants to Regency Mall Santa Land 
+diversity_file = []
+
+names.length.times do |i|
+	diversity_file << Santa.new(genders[i], ethnicities[i])
+	puts "Applicant No. #{diversity_file.length}, #{names[i]} is #{genders[i]}, and #{ethnicities[i]}"
+	puts "-------"
+end
+
+
+
+
+
