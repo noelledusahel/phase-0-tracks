e8c9d1bb0f41c8b566d8f1e7cdedad3b071b2c7f
noelledusahel <barberossa@gmail.com>
2016-11-12 22:00:25 -0500

    complete todo list with rspec

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,36 +1,23 @@
-class TodoList 
+class TodoList
 
-	def initialize(chores)
-		# puts "initializing TodoList..."
-		@TodoList_array = []
-		@chore1 = "do the dishes"
-		@chore2 = "mow the lawn"
-		
-	end 
+	def initialize(list_arr)
+		@todo_arr = list_arr
+	end
 
 	def get_items
-		@Todolist_array << "#{@chore1}"
-		@Todolist_array << "#{@chore2}"
-	end 
+		@todo_arr
+	end
 
-	
-# get_items("do the dishes", "mow the lawn")
-# def get_items(x,y)
-# 	TodoList_array << "#{x}"
-# 	Todolist_array << "#{y}"
+	def add_item(item)
+		@todo_arr << item
+	end
 
+	def delete_item(item)
+		@todo_arr.delete(item)
+	end
 
+	def get_item(index)
+		@todo_arr [index]
+	end
 
-# TodoList_array = []
-# def add_chores()
-# TodoList_array.push() 
-# return TodoList_array
- 
-end 
-
-todolist = TodoList.new
-# (["do the dishes", "mow the lawn"])
-
-p todolist
-
-# TodoList = TodoList.new(["do the dishes", "mow the lawn"])
+end
