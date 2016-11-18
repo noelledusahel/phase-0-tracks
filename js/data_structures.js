8f4d7be8556f0d7adde8af73a82b8fe0e10ca819
noelledusahel <barberossa@gmail.com>
2016-11-18 14:18:58 -0500

    add code to add items to array

---
+++ js/data_structures.js
@@ -0,0 +1,14 @@
+var blue_array = ["sky blue", "teal", "navy blue" , "barbie pink"];
+
+console.log(blue_array.push("red"));
+
+console.log(blue_array);
+
+var name_array = [ "lucky", "stumpy" , "wizard of oz" , "aladdin"];
+
+name_array.push("queenie");
+
+console.log(name_array);
+
+	
+
