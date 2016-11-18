afb510652c0113861fdc73d34ed4aa17e5682466
noelledusahel <barberossa@gmail.com>
2016-11-18 15:50:03 -0500

    create array to populate with new car objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,20 +1,38 @@
-var blue_array = ["sky blue", "teal", "navy blue" , "barbie pink"];
+// var blue_array = ["sky blue", "teal", "navy blue" , "barbie pink"];
 
-blue_array.push("red");
+// blue_array.push("red");
 
-console.log(blue_array);
+// console.log(blue_array);
 
-var name_array = [ "lucky", "stumpy" , "wizard of oz" , "aladdin"];
+// var name_array = [ "lucky", "stumpy" , "wizard of oz" , "aladdin"];
 
-name_array.push("queenie");
+// name_array.push("queenie");
 
-console.log(name_array);
+// console.log(name_array);
 
-var horse = {color: blue_array, name: name_array};
+// // var horse = {color: blue_array, name: name_array};
 
-for (var i = 0; i < name_array.length; i++){
-	console.log("name and color: " + name_array[i], blue_array[i]);
+// for (var i = 0; i < name_array.length; i++){
+// 	console.log("name and color: " + name_array[i], blue_array[i]);
+// };
+// // horse.push(blue_array[1],name_array[1]);
+// // console.log(horse);
+
+var car = {
+  car_class: 'SUV',
+  color: 'black',
+  automatic: true
 };
-// horse.push(blue_array[1],name_array[1]);
-// console.log(horse);
 
+//  constructor function for a car.
+function carAdd(classCar, clr, transmission) {
+  this.classCar = classCar;
+  this.clr = clr;
+  this.transmission = transmission;
+  // car_add.push 
+  // inventory.push({car_class: class, color: clr, automatic: transmission });
+  // car.push(clr, transmission, car_add);
+}; // function does NOT end with semicolon
+// person.push({age: 35, name: "John Doe"}); // Array now contains 1 item
+carAdd('convertable', 'green', false);
+// console.log(inventory);
