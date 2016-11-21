edd3ffcad9cc299199a8c373c9e24fd868461d60
noelledusahel <barberossa@gmail.com>
2016-11-21 11:31:53 -0500

    make a reversal function work

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,15 @@
 //This function will take a string as a parameter and reverse the string. 
 var reverse = function(string){
 // for each letter in the string, we care changing the index position. 
-for (var i = 0; i <string.length; i++){
-	string[i] = string[-i];
-	console.log('' + string[i]);
+var stringArray = string.split("");
+// for (var i = 0; i < stringArray.length; i++){
+// 	stringArray.splice(i, 1);
+// 	console.log(stringArray);
+// };
+var newArray = stringArray.reverse();
+var newWord = newArray.join('');
+console.log(newWord);
 }
-}
-console.log(reverse("hello"));
+
+
+console.log(reverse("hello"))
