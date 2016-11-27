893bde547553313be8266527d6abd9a8b6c2e4fa
noelledusahel <barberossa@gmail.com>
2016-11-27 15:23:12 -0500

    add a block that runs .next on every constanent

--- ruby/alias_manager_squirrels.rb
+++ ruby/alias_manager_squirrels.rb
@@ -3,11 +3,27 @@ def name_encoder(original_name) # the method takes a string as a parameter
 	two_part_name = original_name.split(' ') # splits the name in to two items in an array called two_part_name
 	two_part_name = two_part_name.reverse    # reverse item order in two part name array
 	name_array = two_part_name.join(' ').chars
-	name_array 
+	new_name_array = []
+	
+
+	name_array.each do |i|
+	# num = 0 
+	# while num < name_array.length
+		if i == " "
+			new_name_array << " "
+		elsif i =~ /[aeiou]/
+			new_name_array << i
+		else
+			new_name_array << i.next
+		end 	
+	end 
+	new_name_array
+
 end
 
 
 p name_encoder("Noelle Barber")
+
 
 
 
