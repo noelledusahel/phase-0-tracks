a580979895e6cf85306efc9096a7b65b0a5d45c2
noelledusahel <barberossa@gmail.com>
2016-11-27 08:47:26 -0500

    create a food pyramid hash populated by arrays

---
+++ ruby/nested_data_structures_squirrels.rb
@@ -0,0 +1,7 @@
+food_pyramid = {
+	'Vegetables' => ['lettuce','carrots','string beans', 'collards'],
+	:dairy => ['yogurt','milk','cheese',],
+	:meats => ['chicken','fish','beef','goat','lamb'];
+	'Fruits' => ['papaya','oranges','avocado','watermelon']
+}
+
