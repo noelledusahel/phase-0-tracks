58be2d08fd24fa8c9ab60bc0dbca34d6486bd9ce
noelledusahel <barberossa@gmail.com>
2016-11-27 09:13:05 -0500

    begin to build name encoder method, step 1: break name in to two items in array

---
+++ ruby/alias_manager_squirrels.rb
@@ -0,0 +1,11 @@
+
+def name_encoder(original_name) # the method takes a string as a parameter
+	two_part_name = original_name.split(' ') # splits the name in to two items in an array called two_part_name
+end
+
+
+p name_encoder("Noelle Barber")
+
+
+
+
