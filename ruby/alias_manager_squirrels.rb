289bf915519e8075d73bea0413c04df1c3af624c
noelledusahel <barberossa@gmail.com>
2016-11-27 09:20:22 -0500

    add step to names in to an array of letters

--- ruby/alias_manager_squirrels.rb
+++ ruby/alias_manager_squirrels.rb
@@ -1,6 +1,9 @@
 
 def name_encoder(original_name) # the method takes a string as a parameter
 	two_part_name = original_name.split(' ') # splits the name in to two items in an array called two_part_name
+	two_part_name = two_part_name.reverse    # reverse item order in two part name array
+	name_array = two_part_name.join(' ').chars
+	name_array 
 end
 
 
