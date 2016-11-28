e498b71b2d78366cbd13269dd7877d97cbf272a9
noelledusahel <barberossa@gmail.com>
2016-11-27 21:41:23 -0500

    add pseudocode

--- ruby/problem_solving_methods_squirrels.rb
+++ ruby/problem_solving_methods_squirrels.rb
@@ -1,5 +1,5 @@
-def search_array(array, search)
-	i = 0
+def search_array(array, search) # method takes two parameters 
+	i = 0 						# start the counter at zero so that i can be = to index number
 	while i < array.length 
 		if 
 			array[i].eql?(search) == true
@@ -13,5 +13,5 @@ end
 
 array = [42, 89, 23, 1]
 p search_array(array , 6)
-p search_array(array , 42)
+p search_array(array , 42git )
 p search_array(array , 89)
