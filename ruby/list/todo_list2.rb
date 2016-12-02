70d5fae436ca3c767ca210d4138c468d04675efc
noelledusahel <barberossa@gmail.com>
2016-12-02 17:41:17 -0500

    complete release 3

--- ruby/list/todo_list2.rb
+++ ruby/list/todo_list2.rb
@@ -12,7 +12,19 @@ class TodoList
 		@array.push(str)
 	end
 
+	def delete_item(str)
+		@array.delete(str)
+		@array
+	end
+
+	def get_item(i)
+		@array[i]
+	end 
 
 end
 
-# TodoList.new(["do the dishes", "mow the lawn"])
+list = TodoList.new(["do the dishes", "mow the lawn"])
+p list.get_items
+p list.add_item("mop")
+p list.delete_item("do the dishes")
+p list.get_item(0)
