fbe436ea04c91de674ff976e030bc7df73a0a472
noelledusahel <barberossa@gmail.com>
2016-12-02 11:57:47 -0500

    add driver code to call new instances of our classes using mixin module methods

--- ruby/shout_squirrels.rb
+++ ruby/shout_squirrels.rb
@@ -1,13 +1,37 @@
 module Shout
-	def self.yell_angrily(words) #the methods in standalone modules must be defined on the self keyword
-		words + "!!!" + " :("
-	end 
+	def yell_angrily(words)
+		p words + "!!!" 
+	end
 
-	def self.yell_happily(words)
-		words + " yeeeehaw!!"
+	def yell_happily(words)
+		p words + " yay!!!"
 	end 
 end
 
+class Mom
+	include Shout
+end 
 
-p Shout.yell_happily("Lets Go Cowboys")
-p Shout.yell_angrily("Dammit")
+class Cheerleader
+	include Shout
+end 
+
+darlene = Mom.new
+darlene.yell_angrily("Noelle") 
+
+stacy = Cheerleader.new
+stacy.yell_happily("Lets Go Jaguars")
+
+# module Shout
+# 	def self.yell_angrily(words) #the methods in standalone modules must be defined on the self keyword
+# 		words + "!!!" + " :("
+# 	end 
+
+# 	def self.yell_happily(words)
+# 		words + " yeeeehaw!!"
+# 	end 
+# end
+
+
+# p Shout.yell_happily("Lets Go Cowboys")
+# p Shout.yell_angrily("Dammit")
