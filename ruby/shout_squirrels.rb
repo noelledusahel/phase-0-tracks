a60fe330622ef88eb799ba60d6199f006f4c3cac
noelledusahel <barberossa@gmail.com>
2016-12-02 11:22:46 -0500

    add two method and driver code to call methods

--- ruby/shout_squirrels.rb
+++ ruby/shout_squirrels.rb
@@ -1,3 +1,13 @@
 module Shout
+	def self.yell_angrily(words) #the methods in standalone modules must be defined on the self keyword
+		words + "!!!" + " :("
+	end 
 
+	def self.yell_happily(words)
+		words + " yeeeehaw!!"
+	end 
 end
+
+
+p Shout.yell_happily("Lets Go Cowboys")
+p Shout.yell_angrily("Dammit")
