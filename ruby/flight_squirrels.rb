b5d33d42ffb90bb1024bcbd591188fde9d74969c
noelledusahel <barberossa@gmail.com>
2016-12-02 11:47:33 -0500

    add mixin module to create instance methods

--- ruby/flight_squirrels.rb
+++ ruby/flight_squirrels.rb
@@ -1,5 +1,20 @@
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude}..."
+		# when writing a module as a mixin to add instance methods to a class we dont use self keyword
+	end
+end 
+
 class Bird
+	include Flight
 end
 
 class Plane
+	include Flight
 end
+
+bird = Bird.new
+bird.take_off(800)
+
+place = Plane.new
+place.take_off(30000)
