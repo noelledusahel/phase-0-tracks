93d930eb30b01ac631755f170087c24d20819dd5
noelledusahel <barberossa@gmail.com>
2016-12-08 13:27:20 -0500

    create name and color arrays

---
+++ js/data_structures_squirrels.js
@@ -0,0 +1,9 @@
+
+var colors = ["red", "ombre","sienna","orange"];
+var names = ["Tinkerbell" , "Aladdin", "Snow White" , "Mickey Mouse"];
+
+colors.push("yellow");
+names.push("Sammie");
+
+console.log(colors);
+console.log(names);
