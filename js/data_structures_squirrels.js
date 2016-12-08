acd1daf818a4633d27f56ee9b2a4e7b3a2e5c1ba
noelledusahel <barberossa@gmail.com>
2016-12-08 15:42:17 -0500

    complete function to build new cars

--- js/data_structures_squirrels.js
+++ js/data_structures_squirrels.js
@@ -1,36 +1,38 @@
 
-var colors = ["red", "ombre","sienna","orange"]; //these will be values
-var names = ["Tinkerbell" , "Aladdin", "Snow White" , "Mickey Mouse"]; //these will be keys 
+// var colors = ["red", "ombre","sienna","orange"]; //these will be values
+// var names = ["Tinkerbell" , "Aladdin", "Snow White" , "Mickey Mouse"]; //these will be keys 
 
-colors.push("yellow");
-names.push("Sammie");
+// colors.push("yellow");
+// names.push("Sammie");
 
-console.log(colors);
-console.log(names);
+// console.log(colors);
+// console.log(names);
+// // Release # 1
+// var barn = {};
+// 	for (var i = 0; i < names.length; i++) { 
+// 		barn[names[i]] = colors[i];
+// 	}
+// console.log(barn);
+
+// Release #2 
+function Carbuilder(model, color, automatic){
+	this.model = model;
+	this.color = color; 
+	this.automatic = automatic;
+
+	this.unlock = function(){console.log("Beep Beep"); };
+}
+
+var mazda = new Carbuilder("Miata", "Green", true);
+mazda.unlock();
+console.log(mazda);
+
+var landrover = new Carbuilder("GT", "Red", false);
+console.log(landrover)
+
+var ferrari = new Carbuilder("Spider", "black", false)
+console.log(ferrari)
 
 
 
-// barn.names[1] = colors[1];
-
-// console.log(names[0]);
-
-// barn = {name: names, color: colors};  
-// function stable(name_array, color_array){
-	var barn = {};
-	for (var i = 0; i < names.length; i++) { 
-		barn[names[i]] = colors[i];
-	}
-	console.log(barn);
-	// return barn;
-// } 
-// var result = stable(names, colors);
-// console.log(result);
-
-
-
-// for (var i = 0; i < name_array.length; i++){
-// 	console.log("name and color: " + name_array[i], blue_array[i]);
-// };
-// // horse.push(blue_array[1],name_array[1]);
-// // console.log(horse);
 
