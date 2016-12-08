3a2868597a974688a0fd6420dd796f1e8aad7755
noelledusahel <barberossa@gmail.com>
2016-12-08 01:00:56 -0500

    refactor predicted deaths

--- ruby/gps6/my_solution_squirrels.rb
+++ ruby/gps6/my_solution_squirrels.rb
@@ -38,18 +38,22 @@ class VirusPredictor
   private
 #input is elements of STATE_DATA hash, output is a integer representing
 #the number of predicted deaths. .floor rounds the number down. the return value is number_of_deaths
+   def death_percentage(num)
+    (@population * num).floor
+  end #see if you can refactor this more
+
   def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = death_percentage(0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      number_of_deaths = death_percentage(0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      number_of_deaths = death_percentage(0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      number_of_deaths = death_percentage(0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = death_percentage(0.05)
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
