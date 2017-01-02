cff458003e97174836fa2d1592f4c795299f9107
noelledusahel <barberossa@gmail.com>
2017-01-02 15:38:39 -0500

    create a get route with route parameters

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -68,5 +68,9 @@ get '/great_job' do
 	end 
 end 
 
-
-
+get '/add/:number_1/:number_2' do 
+	number_1 = params[:number_1].to_i
+	number_2 = params[:number_2].to_i
+	result = number_1 + number_2
+	 "the answer is #{result}"
+end 
